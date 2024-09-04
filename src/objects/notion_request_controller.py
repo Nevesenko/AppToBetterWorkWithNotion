fb28ed6bdf86084e821675b9c8967c3e7aedd744
kftoen <nnnevesenko@gmail.com>
2024-09-04 21:40:17 +0300

    Add new class

---
+++ src/objects/notion_request_controller.py
@@ -0,0 +1,19 @@
+
+class NotionRequestController():
+    def __init__(self):
+        pass
+
+    def parse_highlighted_text(self):
+        pass
+
+    def add_new_note(self):
+        pass
+
+
+    def create_mermaid_graph(self):
+        pass
+
+
+    def update_properties(self):
+        '''смотрит на отмеченные галочками строки и придает им
+        новые свойства'''
